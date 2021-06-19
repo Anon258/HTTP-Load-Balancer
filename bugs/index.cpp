3afadb2473ff74f0b3c924177014671051c41a93
maverick6130 <daror6130@gmail.com>
2021-06-19 13:47:47 +0530

    added a better example using the minimal_client module, might help in debugging

--- bugs/index.cpp
+++ bugs/index.cpp
@@ -1,5 +1,6 @@
-#include "../src/http_client.hpp"
+#include "../src/minimal_httpclient.hpp"
 #include <iostream>
+#include <unistd.h>
 
 /* Run along with the test server to observe weird behaviour in i variable*/
 
@@ -8,8 +9,7 @@ int main()
     std::string res;
     int rescode;
     curl_global_init(CURL_GLOBAL_ALL);
-    http_client c;
-    c.enable_logging();
+    minimal_httpclient c;
     int i = 0;
 
     // Should update
@@ -19,46 +19,105 @@ int main()
     std::cout << "Code : " << rescode << std::endl;
     std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
 
     std::cout << "Request ID : " << ++i << std::endl;
     std::cout << "Response : " << res << std::endl;
     std::cout << "Code : " << rescode << std::endl;
     std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
 
     std::cout << "Request ID : " << ++i << std::endl;
     std::cout << "Response : " << res << std::endl;
     std::cout << "Code : " << rescode << std::endl;
     std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
+
+    header_map r_hds;
 
     // Doesn't update
     std::cout << "Request ID : " << ++i << std::endl;
-    c.get("http://localhost:5656/", rescode, &res);
+    c.request("http://localhost:8080/lb1", "GET", "", &res, header_map(), &r_hds, rescode);
     std::cout << "Response : " << res << std::endl;
     std::cout << "Code : " << rescode << std::endl;
-    std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
 
     std::cout << "Request ID : " << ++i << std::endl;
-    c.get("http://localhost:5656/", rescode, &res);
+    c.request("http://localhost:8080/lb2", "GET", "", &res, header_map(), &r_hds, rescode);
     std::cout << "Response : " << res << std::endl;
     std::cout << "Code : " << rescode << std::endl;
-    std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
 
     std::cout << "Request ID : " << ++i << std::endl;
-    c.get("http://localhost:5656/", rescode, &res);
+    c.request("http://localhost:8080/lb3", "GET", "", &res, header_map(), &r_hds, rescode);
     std::cout << "Response : " << res << std::endl;
     std::cout << "Code : " << rescode << std::endl;
-    std::cout << "Log : " << c.log() << std::endl;
     res.clear();
-    c.free_log();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
+
+    std::cout << "Request ID : " << ++i << std::endl;
+    c.request("http://localhost:8080/lb3", "GET", "", &res, header_map(), &r_hds, rescode);
+    std::cout << "Response : " << res << std::endl;
+    std::cout << "Code : " << rescode << std::endl;
+    res.clear();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
+
+    std::cout << "Request ID : " << ++i << std::endl;
+    c.request("http://localhost:8080/lb3", "GET", "", &res, header_map(), &r_hds, rescode);
+    std::cout << "Response : " << res << std::endl;
+    std::cout << "Code : " << rescode << std::endl;
+    res.clear();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
+
+    std::cout << "Request ID : " << ++i << std::endl;
+    c.request("http://localhost:8080/lb3", "GET", "", &res, header_map(), &r_hds, rescode);
+    std::cout << "Response : " << res << std::endl;
+    std::cout << "Code : " << rescode << std::endl;
+    res.clear();
+
+    for (auto h : r_hds)
+    {
+        std::cout << h.first << " : " << h.second;
+    }
+    r_hds.clear();
+    usleep(500000);
+    std::cout << std::endl;
 
     curl_global_cleanup();
 }
